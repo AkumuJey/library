15e65b9dbc266af48bcc29d3080cff7d5d11fdf4
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-09-14 22:31:51 +0300

    Restarting the server with atlas

---
+++ routes/authors.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+// All authors
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+router.get('/new', (req, res) => {
+    res.render('authors/new')
+})
+
+// Create auther
+router.post('/', (req, res) => {
+    res.send('Create')
+})
