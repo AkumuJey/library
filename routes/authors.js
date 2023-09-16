53c65775403cb8b7bf168a733b2f35ddd4f4821c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-09-16 12:58:06 +0300

    Initialized modules author object

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 // All authors
 router.get('/', (req, res) => {
@@ -7,7 +8,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create auther
